98330df9700082c8f517262c8d16105766624de3
om wankhede <omwankhede2002@gmail.com>
2024-03-28 15:41:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/omwankhede8.github.io/",
+  base: "/ShoppingCart/",
   build: {
     outDir: "./docs",
   },
